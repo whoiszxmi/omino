e4a9a164a416618eca850a7520bc3c07d45697ee
Diogo <diogoas13@hotmail.com>
2026-02-27 06:52:16 -0300

    fix server auth anda cookies logic

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,14 +1,29 @@
-import { createClient as createSupabaseClient } from "@supabase/supabase-js";
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+export async function createClient() {
+  const cookieStore = await cookies();
 
-export function createClient() {
-  return createSupabaseClient(supabaseUrl, supabaseAnonKey, {
-    auth: {
-      persistSession: false,
-      autoRefreshToken: false,
-      detectSessionInUrl: false,
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll();
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options),
+            );
+          } catch {
+            // O método `setAll` foi chamado de um Server Component.
+            // Isso pode ser ignorado se você tiver middleware atualizando
+            // cookies dos usuários.
+          }
+        },
+      },
     },
-  });
+  );
 }
